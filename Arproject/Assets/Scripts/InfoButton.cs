80245cca69c6aab9c379fd94820868c03690c574
Julia Sporre <juliasp@kth.se>
2016-11-19 13:05:17 +0100

    Scene with vrbutton showing info

---
+++ Arproject/Assets/Scripts/InfoButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using System;
+using UnityEngine.UI;
+
+public class InfoButton : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject infoButton;
+    public GameObject userDisplay;
+    // Use this for initialization
+    void Start()
+    {
+        infoButton = GameObject.Find("infoButton");
+        infoButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //userDisplay = GameObject.Find("userDisplay");
+    }
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        userDisplay.GetComponent<Text>().text = "HERE IS SOM AWESOME INFORMATION" ;
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Released");
+        userDisplay.GetComponent<Text>().text = "";
+    }
+
+
+	
+
+}

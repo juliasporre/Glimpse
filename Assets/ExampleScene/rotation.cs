9b6b920672082462ab81936c46f71b7f761d488e
Robin Tillman <robin.tillman92@gmail.com>
2016-11-22 13:42:20 +0100

    Merged with Adrias scene

---
+++ Assets/ExampleScene/rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+
+	public float torque;
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.AddTorque(transform.up * torque);
+	}
+	void FixedUpdate() {
+		
+
+	}
+}

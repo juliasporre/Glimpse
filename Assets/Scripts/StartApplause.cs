c8ae84b26053060f99aa94997b5c2a2027408a82
Emilio Lando <emilio.lando@gmail.com>
2016-12-15 00:21:45 +0100

    changed some textures, put out some people and houses

---
+++ Assets/Scripts/StartApplause.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+    /*
+    <summary>
+    Starts the animation at random time
+    </summary>
+    */
+
+    public class StartApplause : MonoBehaviour
+    {
+        /*
+        <summary>
+        Index of the layer of the default state
+        </summary>
+        */
+        [Tooltip("Index of the layer of the default state")]
+        public int _layerIndex = 0;
+        
+        void Start()
+        {
+
+        Invoke("startAnimation", Random.Range(0f,3f));
+            
+        
+        }
+
+    private void startAnimation()
+    {
+       
+   
+        GetComponent<Animation>().Play();
+    }
+        
+    }

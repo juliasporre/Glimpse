6966d4bcf1d1635634b4089337fefa69f0ea86db
Björn Englesson <engles@kth.se>
2016-12-14 11:18:43 +0100

    Added sound

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+
+	private AudioSource townAudio;
+	private AudioSource audienceAudio;
+	private AudioSource birdAudio;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource[] audios = GetComponents<AudioSource> ();
+		townAudio = audios [0];
+		birdAudio = audios [1];
+		audienceAudio = audios [2];
+
+
+		foreach (AudioSource audio in audios)
+			audio.Stop();
+
+
+		townAudio.Play ();
+		birdAudio.Play ();
+		audienceAudio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

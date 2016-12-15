ff85af3d2df2f468d917982e0376e7f98286ddf0
Björn Englesson <engles@kth.se>
2016-12-15 14:02:30 +0100

    Added Adrias super cool new ship

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,29 +3,44 @@ using System.Collections;
 
 public class AudioController : MonoBehaviour {
 
+	private AudioSource[] audios;
 	private AudioSource townAudio;
 	private AudioSource audienceAudio;
 	private AudioSource birdAudio;
 
+	private Vector3 cameraPos;
+	private Vector3 windowPos;
+
+
 	// Use this for initialization
 	void Start () {
-		AudioSource[] audios = GetComponents<AudioSource> ();
+		audios = GetComponents<AudioSource> ();
 		townAudio = audios [0];
 		birdAudio = audios [1];
 		audienceAudio = audios [2];
 
 
-		foreach (AudioSource audio in audios)
-			audio.Stop();
+		foreach (AudioSource audio in audios) {
+			audio.Play ();
+			audio.volume = 0f;
+		}
 
-
-		townAudio.Play ();
-		birdAudio.Play ();
-		audienceAudio.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		cameraPos = GameObject.Find ("ARCamera").transform.position;
+		windowPos = GameObject.Find ("woodenframe2").transform.position;
 	
+		float distance = Vector3.Distance (cameraPos, windowPos);
+
+
+		if (distance < 5f) {
+			foreach (AudioSource audio in audios) {
+				audio.volume = 1.0f - distance / 5f;
+				audio.Play ();
+			}
+		}
 	}
 }
--- Assets/Scripts/InfoButton.cs
+++ Assets/Scripts/InfoButton.cs
@@ -6,25 +6,25 @@ using UnityEngine.UI;
 
 public class InfoButton : MonoBehaviour, IVirtualButtonEventHandler {
 
-    private GameObject infoButton;
-    public GameObject userDisplay;
+//    private GameObject infoButton;
+//    public GameObject userDisplay;
     // Use this for initialization
     void Start()
     {
-        infoButton = GameObject.Find("infoButton");
-        infoButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
-        //userDisplay = GameObject.Find("userDisplay");
+//        infoButton = GameObject.Find("infoButton");
+//		infoButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+//        //userDisplay = GameObject.Find("userDisplay");
     }
     public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
     {
         Debug.Log("Pressed");
-        userDisplay.GetComponent<Text>().text = "HERE IS SOM AWESOME INFORMATION" ;
+//        userDisplay.GetComponent<Text>().text = "HERE IS SOM AWESOME INFORMATION" ;
     }
 
     public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
     {
         Debug.Log("Released");
-        userDisplay.GetComponent<Text>().text = "";
+//        userDisplay.GetComponent<Text>().text = "";
     }
 
 

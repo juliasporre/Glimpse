f28063813da68ba0dac5f0e2b313f511a8ecda62
Björn Englesson <engles@kth.se>
2016-12-15 17:15:00 +0100

    Fixed stuff in scene

---
+++ Assets/treeScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class treeScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Start () {
+		transform.LookAt (GameObject.Find("woodenframe2").transform.position);
+	}
+}
